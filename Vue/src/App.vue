5715d334d4dc34b6b4cc02b145507f4d64d9caf0
wf <1982392655@qq.com>
2023-11-07 18:28:10 +0800

    更新 iconfont

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -8,5 +8,5 @@
 @import url("./style/index.css"); /** 公共样式 */
 @import url("./style/elementPlus.css"); /** 样式覆盖文件 */
 @import url("//at.alicdn.com/t/c/font_4123658_2df5193vn43.css"); /** 在线字体文件 */
-@import url("./iconfont/font_4123658_cez4tm11q06/iconfont.css");
+@import url("./iconfont/font_4123658_x4022ql41vj/iconfont.css");
 </style>
