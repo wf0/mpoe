9bd2d6f67ace0bc145185d0c75687605c3f434a6
wf <1982392655@qq.com>
2023-11-12 16:13:48 +0800

    新增 y-websocket 服务器实现

--- Node/ws/yjsServer.js
+++ Node/ws/yjsServer.js
@@ -5,6 +5,7 @@ module.exports = () => {
 
   // 创建 yjs ws 服务
   const yjsws = new WebSocketServer({ port: 8000 });
+
   yjsws.on("connection", (conn, req) => {
     /**
      * 这里要控制相同文件的用户才广播事件 ,不给自己发送数据
