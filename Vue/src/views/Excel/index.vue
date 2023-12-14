22ccb339d38e2d265dd296ee9860540088ef269b
wf <1982392655@qq.com>
2023-12-14 09:33:18 +0800

    新增setSheetAdd success 回调

--- Vue/src/views/Excel/index.vue
+++ Vue/src/views/Excel/index.vue
@@ -47,6 +47,9 @@ const importFileHandle = (e) => {
       // sheet 便是每一个 sheet 页，需要根据实际的数量动态创建
       luckysheet.setSheetAdd({
         sheetObject: sheet,
+        success: () => {
+          console.log("success");
+        },
       });
     });
     // 清空
@@ -118,45 +121,7 @@ onMounted(async () => {
       },
     },
   };
-  luckysheet.create({
-        container: "luckysheet",
-        title: "测试Excel", // 表 头名
-        lang: "zh", // 中文
-        showtoolbar: true, // 是否显示工具栏
-        showinfobar: false, // 是否显示顶部信息栏
-        showsheetbar: true, // 是否显示底部sheet按钮
-        cellRightClickConfig : {
-          copy: true, // 复制
-          copyAs: false, // 复制为
-          paste: false, // 粘贴
-          insertRow: false, // 插入行
-          insertColumn: false, // 插入列
-          deleteRow: false, // 删除选中行
-          deleteColumn: false, // 删除选中列
-          deleteCell: false, // 删除单元格
-          hideRow: false, // 隐藏选中行和显示选中行
-          hideColumn: false, // 隐藏选中列和显示选中列
-          rowHeight: false, // 行高
-          columnWidth: false, // 列宽
-          clear: false, // 清除内容
-          matrix: false, // 矩阵操作选区
-          sort: false, // 排序选区
-          filter: false, // 筛选选区
-          chart: false, // 图表生成
-          image: false, // 插入图片
-          link: false, // 插入链接
-          data: false, // 数据验证
-          cellFormat: false,// 设置单元格格式
-          customs: [
-            {
-              title: "自定义右键菜单",
-              onClick: function (clickEvent, event, params) {
-                console.log("function test click", clickEvent, event, params);
-              },
-            },
-          ],
-        }
-      });
+  luckysheet.create(options);
 });
 </script>
 
