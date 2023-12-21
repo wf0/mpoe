ef028a87262085b07fce2d1acd8e61d48a176de5
wf <1982392655@qq.com>
2023-12-21 17:40:50 +0800

    修改 options

--- Vue/src/views/Excel/index.vue
+++ Vue/src/views/Excel/index.vue
@@ -84,27 +84,6 @@ onMounted(async () => {
         : "/excel?fileid=" + fileid,
     updateUrl: "ws://localhost:9000?fileid=" + fileid, // 实现传参,
     cellRightClickConfig: {
-      copy: true, // 复制
-      copyAs: false, // 复制为
-      paste: false, // 粘贴
-      insertRow: false, // 插入行
-      insertColumn: false, // 插入列
-      deleteRow: false, // 删除选中行
-      deleteColumn: false, // 删除选中列
-      deleteCell: false, // 删除单元格
-      hideRow: false, // 隐藏选中行和显示选中行
-      hideColumn: false, // 隐藏选中列和显示选中列
-      rowHeight: false, // 行高
-      columnWidth: false, // 列宽
-      clear: false, // 清除内容
-      matrix: false, // 矩阵操作选区
-      sort: false, // 排序选区
-      filter: false, // 筛选选区
-      chart: false, // 图表生成
-      image: false, // 插入图片
-      link: false, // 插入链接
-      data: false, // 数据验证
-      cellFormat: false, // 设置单元格格式
       customs: [
         {
           title: "自定义右键菜单",
@@ -113,12 +92,6 @@ onMounted(async () => {
           },
         },
       ],
-    },
-    hook: {
-      cellEditBefore() {
-        console.log("cellEditBefore");
-        return false;
-      },
     },
   };
   luckysheet.create(options);
